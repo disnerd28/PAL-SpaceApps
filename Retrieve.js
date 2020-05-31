a877d43946650d56da9dc43da185483b1ad51e16
disnerd28 <60379504+disnerd28@users.noreply.github.com>
2020-05-31 05:40:15 -0500

    Add csv and update csv readings

--- Retrieve.js
+++ Retrieve.js
@@ -5,10 +5,14 @@ const Retrieve = {
 const axios = require('axios');
 const CSVToJSON = require('csvtojson');
 
-// csv corn yield per acre
+// csv particulate matter data
 Retrieve.PMdata = async function() {
-	csvPMdata = await CSVToJSON().fromFile('//') //csv file name
-	return PMdata[5].estimate; //index = row number, .estimate = column header
+	csvPMdata = await CSVToJSON().fromFile('epaPM2016.csv') //csv file name
+  var i;
+  for (i = 0; i < 121; i++) {
+    rawPM[i] = PMdata[i].Conc;  //index = row number, .Conc = column header
+  }
+	return rawPM;
 }
 
 module.exports = Retrieve;
--- index.js
+++ index.js
@@ -8,8 +8,19 @@ const Retrieve = require('./Retrieve.js');
 
 async function gradeAirQuality() {
   //get data from csv file
+  csvPMdata = await CSVToJSON().fromFile('epaPM2016.csv') //csv file name
+  var i;
+  for (i = 0; i < 121; i++) {
+    rawPM[i] = csvPMdata[i].Conc;  //index = row number, .Conc = column header
+  }
 
-  // compare user input to calculated csv total and return grade for pesticides
+  // compare NASA data to set air quality standard
+  if (rawPM[i] < 10)
+		return "C"; //
+	else if (rawPM[i] >= 10  & rawPM[i] < 50)
+		return "B"; //
+	else if (rawPM[i] >= 50)
+		return "A"; //
 
 }
 
