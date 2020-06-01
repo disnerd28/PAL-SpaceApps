4a0b50095df27cb4ff07dcdc194264d4a89a5f8c
disnerd28 <60379504+disnerd28@users.noreply.github.com>
2020-05-31 21:16:58 -0500

    Finalizing code and reorganized CAD files

---
+++ Python Ver/additionalMonitoring.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 31 20:26:14 2020
+
+@author: Lan Bui
+"""
+
+import csv
+
+#open csv file and go through data 
+file_epaNO2 = open('epaNO2.csv')
+file_epaPM2_5 = open('epaPM2_5.csv')
+data_epaNO2 = csv.reader(file_epaNO2)
+data_epaPM2_5 = csv.reader(file_epaPM2_5)
+
+#skip header in csv file
+next(data_epaNO2)
+next(data_epaPM2_5)
+
+#put csv elements into a list to simplify analysis
+list_epaNO2 = list(data_epaNO2)
+list_epaPM2_5 = list(data_epaPM2_5)
+
+#read through NO2 data 
+for row in list_epaNO2:
+    #reading from csv is str so convert to float for if else statements
+    no2Conc = float(row[4])
+    
+    #print concentration value and respective power setting
+    if 100 < no2Conc:
+       print(str(no2Conc) + " ppb = Please keep the air purfier on at full capacity. The air outdoors has very high levels of NO2.")
+       print("   It is recommended that everyone be wary of the time spent outdoors as long exposure may be harmful.\n")
+    elif 53 < no2Conc <= 100:
+       print(str(no2Conc) + " ppb = Please set the air purifier at 50% capacity. The air outdoors has some moderate levels of NO2.")
+       print("   It is recommended that patients with underlying health conditions should not go outside if possible.\n")
+    else:
+       print(str(no2Conc) + " ppb = You can keep the purifier off. The air outdoors seems pretty great today!\n")
+
+#read through particulate matter data     
+for row in list_epaPM2_5:
+    #reading from csv is str so convert to float for if else statements
+    pmConc = float(row[4])
+    
+    #print concentration value and respective power setting
+    if 3.61e-09 < pmConc:
+       print(str(pmConc) + " ug/m3 = Please keep the air purfier on at full capacity. The air outdoors has very high levels of PM.")
+       print("   It is recommended that everyone be wary of the time spent outdoors as long exposure may be harmful.\n")
+    elif 12 < pmConc <= 35:
+       print(str(pmConc) + " ug/m3 = Please set the air purifier at 50% capacity. The air outdoors has some moderate levels of PM.")
+       print("   It is recommended that patients with underlying health conditions should not go outside if possible.\n")
+    else:
+       print(str(pmConc) + " ug/m3 = You can keep the purifier off. The air outdoors seems pretty great today!\n")
--- Python Ver/systemMonitor.py
+++ Python Ver/systemMonitor.py
@@ -11,25 +11,21 @@ import csv
 file_CSV = open('csaNO2.csv')
 data_CSV = csv.reader(file_CSV)
 
-#skip header in csv file
-#next(data_CSV)
-
 #put csv elements into a list to simplify analysis
 list_CSV = list(data_CSV)
  
-count = 0
 for row in list_CSV:
     #reading from csv is str so convert to float for if else statements
     no2Conc = float(row[0])
     
     #print concentration value and respective power setting
-    if no2Conc == -999.0: #no reading
+    if no2Conc == -999.0: #no reading, based on comments from the CSA txt file
        print(str(no2Conc) + " = A reading was not performed")
-    elif no2Conc == -888.0: #reading error
+    elif no2Conc == -888.0: #reading error, based on comments from the CSA txt file
        print(str(no2Conc) + " = There is an error in data collection")
     elif 3.61e-09 < no2Conc:
        print(str(no2Conc) + " = Please keep the air purfier on at full capacity. The air outdoors has very high levels of NO2.")
-       print("   It is recommended that everyone be wary of the time you spend outdoors as long exposure may be harmful.\n")
+       print("   It is recommended that everyone be wary of the time spent outdoors as long exposure may be harmful.\n")
     elif 4.10e-10 < no2Conc <= 3.61e-09:
        print(str(no2Conc) + " = Please set the air purifier at 75% capacity. The air outdoors has some high levels of NO2.")
        print("   It is recommended that patients with underlying health conditions should not leave their room if possible.\n")
@@ -40,4 +36,4 @@ for row in list_CSV:
        print(str(no2Conc) + " = Please set the air purifier at 25% capacity. The air outdoors has some low concentrations of NO2.")
        print("   Going outside would not be harmful, but may be uncomfortable to breathe if you are sensitive.\n")    
     else:
-       print(str(no2Conc) + " = You can keep the purifier off. The air outdoors seems pretty good todday!\n")
+       print(str(no2Conc) + " = You can keep the purifier off. The air outdoors seems pretty great today!\n")
